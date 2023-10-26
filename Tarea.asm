9e59d1e06d435d374728d6e2fcb885711377130c
Nicolas Romero <nicolas.romero.mora@est.una.ac.cr>
2023-10-25 20:49:29 -0600

    Count words and characters are ready

--- Tarea.asm
+++ Tarea.asm
@@ -4,10 +4,10 @@
 .data
     filename db 100 dup(0)
     buffer db 100 dup("$")
-    text db "Mi nombre es Nicolasooo@",0
-    msg db "Ingrese el nombre del archivo: $"
+    text db "Mi nombre es Nicolasooo a @",0
+    msg db "Ingrese el nombre del archivo::: $"
     count db 3 dup(0)
-    newline db 13, 10, "$" ; Newline characters for display
+    newline db 13, 10, "$" 
 
 .code
 start:
@@ -15,17 +15,17 @@ start:
     mov ds, ax
 
     call count_characters
-
+    call count_words
     mov ah, 4ch
     int 21h
 
 count_characters proc
     mov si, offset text
     mov cx, 0
-    mov bx, 0  ; Variable to store the count
+    mov bx, 0  
 loop1:
     mov al, [si]
-    cmp al, '@'  ; Check for the '@' character
+    cmp al, '@'  
     je end_loop1
     cmp al, 'A'
     jb next_char
@@ -37,40 +37,87 @@ loop1:
     jbe letter
 
 letter:
-    inc bx      ; Increment the count
+    inc bx      
 next_char:
     inc si
     jmp loop1
 
 end_loop1:
-    ; Store the count in the 'count' variable
-    mov al, bl  ; Copy the count from bx to al
+   
+    mov al, bl  
     mov count, al
 
-    ; Convert the count (in al) to an ASCII string
-    mov ah, 0   ; Clear ah
+   
+    mov ah, 0   
     mov cx, 10
-    mov di, offset buffer + 9  ; Start storing digits from the end of the buffer
+    mov di, offset buffer + 9 
 
 convert_loop:
-    xor dx, dx   ; Clear any previous remainder
-    div cx       ; Divide AL by 10, result in AL, remainder in DX
-    add dl, '0'  ; Convert remainder to ASCII
-    dec di       ; Move DI backward in the buffer
-    mov [di], dl ; Store the ASCII digit in the buffer
-    test al, al  ; Check if AL is zero
+    xor dx, dx   
+    div cx      
+    add dl, '0' 
+    dec di       
+    mov [di], dl 
+    test al, al  
     jnz convert_loop
 
-    ; Display the converted count (ASCII string)
+
     mov ah, 9
     lea dx, [di]
     int 21h
 
-    ; Display a newline
     lea dx, newline
     int 21h
 
     ret
 count_characters endp
+
+count_words proc
+    mov si, offset text
+    mov cx, 0
+    mov bx, 0
+    mov dx, 0
+
+word_loop:
+    mov al, [si]
+    cmp al, '@'
+    je end_word_loop
+    cmp al, ' ' 
+    je space_found
+    jmp next_char_W
+
+space_found:
+    inc bx 
+
+next_char_W:
+    inc si
+    jmp word_loop
+
+end_word_loop:
+    mov al, bl 
+    mov count, al
+
+    mov ah, 0
+    mov cx, 10
+    mov di, offset buffer + 9
+
+convert_loop_W:
+    xor dx, dx
+    div cx
+    add dl, '0'
+    dec di
+    mov [di], dl
+    test al, al
+    jnz convert_loop_W
+
+    mov ah, 9
+    lea dx, [di]
+    int 21h
+
+    lea dx, newline
+    int 21h
+
+    ret
+count_words endp
 
 end
